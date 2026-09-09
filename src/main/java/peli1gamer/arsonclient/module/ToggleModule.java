dad63905d89de4281dc429105fc81f00be35e7ea
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 21:48:38 +0300

    Rewrite Arson Client core architecture

---
+++ src/main/java/peli1gamer/arsonclient/module/ToggleModule.java
@@ -0,0 +1,11 @@
+package peli1gamer.arsonclient.module;
+
+public abstract class ToggleModule implements Module {
+    private boolean enabled;
+    @Override public final boolean enabled() { return enabled; }
+    @Override public final void setEnabled(boolean value) {
+        if (enabled == value) return;
+        enabled = value;
+        if (value) onEnable(); else onDisable();
+    }
+}
