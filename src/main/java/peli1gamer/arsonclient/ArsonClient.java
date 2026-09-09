3784a4da52462fec4ba2c270fc397b50c93dec7c
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 23:37:14 +0300

    Wire HUD and main-menu GUI entry points

--- src/main/java/peli1gamer/arsonclient/ArsonClient.java
+++ src/main/java/peli1gamer/arsonclient/ArsonClient.java
@@ -2,8 +2,14 @@ package peli1gamer.arsonclient;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import peli1gamer.arsonclient.gui.ArsonGui;
+import peli1gamer.arsonclient.gui.HudOverlay;
 import peli1gamer.arsonclient.module.ModuleManager;
 import peli1gamer.arsonclient.module.impl.AutoTotemModule;
 import peli1gamer.arsonclient.module.impl.ElytraFlyModule;
@@ -33,19 +39,28 @@ public final class ArsonClient implements ClientModInitializer {
         MODULES.register(new FullbrightModule());
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            TARGET_TRACKER.tick();
             TARGETS.tick();
             MODULES.tick();
         });
 
-        LOGGER.info("Arson Client core initialized with {} implemented modules", MODULES.all().size());
+        HudRenderCallback.EVENT.register((graphics, tickCounter) -> HudOverlay.render(graphics));
+
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof TitleScreen) {
+                int width = 120;
+                int x = 8;
+                int y = scaledHeight - 28;
+                screen.addRenderableWidget(Button.builder(
+                    net.minecraft.network.chat.Component.literal("Arson Client"),
+                    button -> ArsonGui.open()
+                ).bounds(x, y, width, 20).build());
+            }
+        });
+
+        LOGGER.info("Arson Client core initialized with {} registered modules", MODULES.all().size());
     }
 
-    public static ModuleManager modules() {
-        return MODULES;
-    }
-
-    public static TargetManager targets() {
-        return TARGETS;
-    }
+    public static ModuleManager modules() { return MODULES; }
+    public static TargetManager targets() { return TARGETS; }
+    public static TargetTracker targetTracker() { return TARGET_TRACKER; }
 }
