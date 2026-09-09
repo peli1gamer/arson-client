d3e52f99fd7686ef98aa7e2a170ad9e182623650
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 23:24:55 +0300

    Fix ArsonClient source newline/import corruption

--- src/main/java/peli1gamer/arsonclient/ArsonClient.java
+++ src/main/java/peli1gamer/arsonclient/ArsonClient.java
@@ -6,20 +6,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import peli1gamer.arsonclient.module.ModuleManager;
 import peli1gamer.arsonclient.module.impl.AutoTotemModule;
-import peli1gamer.arsonclient.module.impl.ElytraFlyModule;\nimport peli1gamer.arsonclient.module.impl.EntityEspModule;\nimport peli1gamer.arsonclient.module.impl.BlockEspModule;\nimport peli1gamer.arsonclient.module.impl.RetaliationTargetModule;
-import peli1gamer.arsonclient.target.TargetManager;\nimport peli1gamer.arsonclient.target.TargetTracker;\nimport peli1gamer.arsonclient.module.impl.SprintModule;\nimport peli1gamer.arsonclient.module.impl.FullbrightModule;
+import peli1gamer.arsonclient.module.impl.ElytraFlyModule;
+import peli1gamer.arsonclient.module.impl.EntityEspModule;
+import peli1gamer.arsonclient.module.impl.BlockEspModule;
+import peli1gamer.arsonclient.module.impl.RetaliationTargetModule;
+import peli1gamer.arsonclient.module.impl.SprintModule;
+import peli1gamer.arsonclient.module.impl.FullbrightModule;
+import peli1gamer.arsonclient.target.TargetManager;
+import peli1gamer.arsonclient.target.TargetTracker;
 
 public final class ArsonClient implements ClientModInitializer {
     public static final String MOD_ID = "arson-client";
     public static final Logger LOGGER = LoggerFactory.getLogger("Arson Client");
     private static final ModuleManager MODULES = new ModuleManager();
-    private static final TargetManager TARGETS = new TargetManager();\n    private static final TargetTracker TARGET_TRACKER = new TargetTracker();
-    @Override public void onInitializeClient() {
+    private static final TargetManager TARGETS = new TargetManager();
+    private static final TargetTracker TARGET_TRACKER = new TargetTracker();
+
+    @Override
+    public void onInitializeClient() {
         MODULES.register(new AutoTotemModule());
-        MODULES.register(new ElytraFlyModule());\n        MODULES.register(new EntityEspModule());\n        MODULES.register(new BlockEspModule());\n        MODULES.register(new RetaliationTargetModule());\n        MODULES.register(new SprintModule());\n        MODULES.register(new FullbrightModule());
-        ClientTickEvents.END_CLIENT_TICK.register(client -> { TARGET_TRACKER.tick(); TARGETS.tick(); MODULES.tick(); });
+        MODULES.register(new ElytraFlyModule());
+        MODULES.register(new EntityEspModule());
+        MODULES.register(new BlockEspModule());
+        MODULES.register(new RetaliationTargetModule());
+        MODULES.register(new SprintModule());
+        MODULES.register(new FullbrightModule());
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            TARGET_TRACKER.tick();
+            TARGETS.tick();
+            MODULES.tick();
+        });
+
         LOGGER.info("Arson Client core initialized with {} implemented modules", MODULES.all().size());
     }
-    public static ModuleManager modules(){return MODULES;}
-    public static TargetManager targets(){return TARGETS;}
+
+    public static ModuleManager modules() {
+        return MODULES;
+    }
+
+    public static TargetManager targets() {
+        return TARGETS;
+    }
 }
