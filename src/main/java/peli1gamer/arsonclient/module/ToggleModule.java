274cc836c014ca0d68b6bd7b5edd2781d6240119
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 23:36:51 +0300

    Make module enable state failure-safe

--- src/main/java/peli1gamer/arsonclient/module/ToggleModule.java
+++ src/main/java/peli1gamer/arsonclient/module/ToggleModule.java
@@ -2,10 +2,18 @@ package peli1gamer.arsonclient.module;
 
 public abstract class ToggleModule implements Module {
     private boolean enabled;
+
     @Override public final boolean enabled() { return enabled; }
+
     @Override public final void setEnabled(boolean value) {
         if (enabled == value) return;
         enabled = value;
-        if (value) onEnable(); else onDisable();
+        try {
+            if (value) onEnable();
+            else onDisable();
+        } catch (Throwable t) {
+            enabled = !value;
+            throw t;
+        }
     }
 }
