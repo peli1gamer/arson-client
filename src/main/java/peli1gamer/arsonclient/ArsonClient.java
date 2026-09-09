5d87b1b4dc61d5377278e26881e6bc10462393db
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 21:55:52 +0300

    Register first implemented Arson modules

--- src/main/java/peli1gamer/arsonclient/ArsonClient.java
+++ src/main/java/peli1gamer/arsonclient/ArsonClient.java
@@ -5,6 +5,8 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import peli1gamer.arsonclient.module.ModuleManager;
+import peli1gamer.arsonclient.module.impl.AutoTotemModule;
+import peli1gamer.arsonclient.module.impl.ElytraFlyModule;
 import peli1gamer.arsonclient.target.TargetManager;
 
 public final class ArsonClient implements ClientModInitializer {
@@ -12,10 +14,11 @@ public final class ArsonClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("Arson Client");
     private static final ModuleManager MODULES = new ModuleManager();
     private static final TargetManager TARGETS = new TargetManager();
-
     @Override public void onInitializeClient() {
+        MODULES.register(new AutoTotemModule());
+        MODULES.register(new ElytraFlyModule());
         ClientTickEvents.END_CLIENT_TICK.register(client -> { TARGETS.tick(); MODULES.tick(); });
-        LOGGER.info("Arson Client core initialized");
+        LOGGER.info("Arson Client core initialized with {} implemented modules", MODULES.all().size());
     }
     public static ModuleManager modules(){return MODULES;}
     public static TargetManager targets(){return TARGETS;}
