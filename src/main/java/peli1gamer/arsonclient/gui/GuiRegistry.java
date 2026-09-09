b4a9395d66d391f695a9e09210e082a3410c71f8
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 21:51:54 +0300

    Add Arson GUI navigation and settings overlay architecture

---
+++ src/main/java/peli1gamer/arsonclient/gui/GuiRegistry.java
@@ -0,0 +1,2 @@
+package peli1gamer.arsonclient.gui;
+public final class GuiRegistry { private static final ArsonGuiController CONTROLLER=new ArsonGuiController(); private GuiRegistry(){} public static ArsonGuiController controller(){return CONTROLLER;} }
